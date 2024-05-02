e383a052ade8d6a0dfccfccfdc100f4bdc479c20
armgaloyan <armaa.galoyan@yandex.ru>
2024-05-02 21:27:38 +0300

    changed jdbc task to hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,14 +5,7 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Util {
-    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "23346897";
     private static SessionFactory sessionFactory = null;
     private Util () {
     }
@@ -25,12 +18,5 @@ public class Util {
                     .buildSessionFactory();
         }
         return sessionFactory;
-    }
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
